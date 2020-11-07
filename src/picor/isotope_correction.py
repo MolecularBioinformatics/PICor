ec6b2c4a39a2d22646b2f7d344f2ca0ee295419a
Joern Dietze <j.dietze@posteo.de>
2020-11-07 06:34:54 +0100

    Move cli functions into separate module

--- src/picor/isotope_correction.py
+++ src/picor/isotope_correction.py
@@ -1,54 +1,11 @@
 """Isotopologue correction using statistical distributions.
 
-Read raw data as csv or excle and correct for natural abundance
-
-Usage:
-    isotope_correction.py FILE METABOLITE [-o OUTFILE]
-                                          [-s COL]...
-                                          [-x EXCOL]...
-                                          [--isotopes-file IFILE]
-                                          [--metabolites-file MFILE]
-    isotope_correction.py FILE METABOLITE --res-correction 
-                                          [--mz-calibration MZ]
-                                          [--isotopes-file IFILE]
-                                          [-o OUTFILE]
-                                          [-s COL]...
-                                          [-x EXCOL]...
-                                          [--resolution RES]
-                                          [--metabolites-file MFILE]
-    isotope_correction.py (-h | --help)
-    isotope_correction.py --version
-
-Arguments:
-    FILE        Path to excle or csv file
-    METABOLITE  Name as in metabolites-file
-
-Options:
-  -o --output OUTFILE       Output file path (csv format)
-  -r --res-correction       Perform resolution correction
-  --resolution RES          Resolution of measurement [default: 60000]
-  --mz-calibration MZ       Mass-charge ratio of calibration point [default: 200]
-  -s --subset COL           Column for calculation; can be used multiple times
-  -x --exclude-col EXCOL    Column to ignore; can be used multiple times
-  --metabolites-file MFILE  Path to tab-separated metabolites file
-                            Name, formula and charge as rows; e.g. Suc C4H4O3 -1
-  --isotopes-file IFILE     Path to tab-separated isotope file
-                            Element, mass, abundance and isotope as rows
-                            E.g. H 1.008 0.99 H01
-  -h --help                 Show this screen.
-  --version                 Show version.
-
+Functions:
+    calc_isotopologue_correction: Correct DataFrame with measurements.
 """
 import os
 import logging
-from pathlib import Path
-import pkg_resources
-import sys
 
-from docopt import docopt
-import pandas as pd
-
-import picor
 import picor.isotope_probabilities as ip
 import picor.resolution_correction as rc
 
@@ -69,7 +26,6 @@ def calc_isotopologue_correction(
     resolution=60000,
     isotopes_file=None,
     metabolites_file=None,
-    verbose=False,
 ):
     """Calculate isotopologue correction factor for metabolite.
 
@@ -97,8 +53,6 @@ def calc_isotopologue_correction(
     :param metabolites_file: Path to metabolites file
         tab-separated file with name, formula and charge as rows
         e.g. Suc C4H4O3 -1
-    :param verbose: bool (default: False)
-        print correction and transition factors
     :return: pandas DataFrame
         Corrected data
     """
@@ -137,8 +91,7 @@ def calc_isotopologue_correction(
         )
         assert corr >= 1, "Correction factor should be greater or equal 1"
         data[label1] = corr * data[label1]
-        if verbose:
-            print(f"Correction factor {label1}: {corr}")
+        _logger.info(f"Correction factor {label1}: {corr}")
         for label2 in subset:
             if ip.label_shift_smaller(label1, label2):
                 if resolution_correction:
@@ -151,48 +104,13 @@ def calc_isotopologue_correction(
                         isotopes_file,
                     )
                     data[label2] = data[label2] - indirect_overlap_prob * data[label1]
-                    if verbose:
-                        print(
-                            f"Overlapping prob {label1} -> {label2}: {indirect_overlap_prob}"
-                        )
+                    _logger.info(
+                        f"Overlapping prob {label1} -> {label2}: {indirect_overlap_prob}"
+                    )
                 trans_prob = ip.calc_transition_prob(
                     label1, label2, metabolite, metabolites_file, isotopes_file
                 )
                 data[label2] = data[label2] - trans_prob * data[label1]
                 data[label2].clip(lower=0, inplace=True)
-                if verbose:
-                    print(f"Transition prob {label1} -> {label2}: {trans_prob}")
+                _logger.info(f"Transition prob {label1} -> {label2}: {trans_prob}")
     return data
-
-
-def main(arguments):
-    """Function for CLI interface."""
-    infile = Path(arguments["FILE"])
-    outfile = arguments["--output"]
-    if infile.suffix == ".xslx":
-        raw_data = pd.read_excel(infile, index_col=0)
-    elif infile.suffix == ".csv":
-        raw_data = pd.read_csv(infile, index_col=0)
-    else:
-        raise ValueError("FILE can be either '.csv' or '.xslx' file type")
-    corr_data = picor.calc_isotopologue_correction(
-        raw_data,
-        arguments["METABOLITE"],
-        subset=arguments["--subset"],
-        exclude_col=arguments["--exclude-col"],
-        resolution_correction=arguments["--res-correction"],
-        mz_calibration=arguments["--mz-calibration"],
-        resolution=arguments["--resolution"],
-        isotopes_file=arguments["--isotopes-file"],
-        metabolites_file=arguments["--metabolites-file"],
-    )
-    if outfile:
-        corr_data.to_csv(outfile)
-    print(corr_data)
-
-
-if __name__ == "__main__":
-    version = pkg_resources.get_distribution("picor").version
-    arguments = docopt(__doc__, version=version)
-    _logger.info(f"{arguments=}")
-    sys.exit(main(arguments))
