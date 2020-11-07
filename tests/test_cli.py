bfd92c33f9279370bcc2ccbc94b49ffa41d07ebf
Joern Dietze <j.dietze@posteo.de>
2020-11-07 08:22:37 +0100

    Add tst case for CLI

---
+++ tests/test_cli.py
@@ -0,0 +1,27 @@
+"""Unit tests for isotope correction."""
+
+import unittest
+import subprocess
+
+
+import pandas as pd
+
+__author__ = "Jørn Dietze"
+__copyright__ = "Jørn Dietze"
+__license__ = "gpl3"
+
+
+class TestCLI(unittest.TestCase):
+    """Call CLI as subprocess."""
+
+    def test_result(self):
+        """Output file result."""
+        outfile = "test_out.csv"
+        res_correct = "./tests/test_dataset_corrected.csv"
+        cmd = f"picor tests/test_dataset.csv NAD -x 'dummy column int' -x 'dummy column str' -o {outfile}"
+        process = subprocess.run(cmd, check=True, shell=True)
+        df_res = pd.read_csv(outfile, index_col=0)
+        df_res.drop(columns=["dummy column str", "dummy column int"], inplace=True)
+        df_corr = pd.read_csv(res_correct, index_col=0)
+        assert process.returncode == 0
+        pd.testing.assert_frame_equal(df_res, df_corr)
