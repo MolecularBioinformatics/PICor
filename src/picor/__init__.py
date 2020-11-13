80ce07bb1ba2c98de914f025fa778b4a7867b804
Joern Dietze <j.dietze@posteo.de>
2020-11-13 08:06:46 +0100

    Remove unncecessary dist_name import checks

--- src/picor/__init__.py
+++ src/picor/__init__.py
@@ -7,13 +7,3 @@ Modules:
     resolution_correction.py: subroutines for resolution depend correction
 """
 from picor.isotope_correction import calc_isotopologue_correction
-from pkg_resources import get_distribution, DistributionNotFound
-
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = "PICor"
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
-    __version__ = "unknown"
-finally:
-    del get_distribution, DistributionNotFound
